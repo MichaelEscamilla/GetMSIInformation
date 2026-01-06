5f7c71b80b2ec8b8adb90c1624c8bc7728094b54
Michael Escamilla <126358188+MichaelEscamilla@users.noreply.github.com>
2026-01-06 10:24:06 -0800

    2026.1.6.0 (#4)

--- GetMSIInformation.ps1
+++ GetMSIInformation.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 2026.1.5.1
+.VERSION 2026.1.6.0
 
 .GUID 3a7b9c4d-2e8f-4a1b-9d6c-5e3f7a8b9c2d
 
@@ -32,6 +32,7 @@
 2024-12.8.0   - Formatted Script for Publishing to PowerShell Gallery
 2026-1.5.0    - Added Icon extraction and export functionality. Added context menu items to open the icon temp folder and right-click menu folder.
 2026.1.5.1    - Fixed a bug when launching the script from the internet
+2026.1.6.0    - Fixed a bug where the 'No Icon' label would not hide
 
 .PRIVATEDATA
 
@@ -64,7 +65,7 @@ param (
 # Script Name
 $Script:ScriptName = "GetMSIInformation.ps1"
 # Script Version
-[System.Version]$Script:ScriptVersion = "2026.1.5.1"
+[System.Version]$Script:ScriptVersion = "2026.1.6.0"
 # Right-Click Menu
 $Script:RightClickMenuName = "Get MSI Information"
 $Script:RightClickMenuFolderPath = "$env:LOCALAPPDATA\GetMSIInformation"
@@ -467,6 +468,9 @@ function Set-IconImageNavigation {
 
   # Set GlobalIndex
   $Script:CurrentIconIndex = $NewIndex
+
+  # Hide the 'No Icon' Label
+  $lbl_NoIcon.Visibility = "Collapsed"
     
   # Hide the current Icon
   ($Script:IconImageControlsList | Where-Object { $_.Index -eq $CurrentIndex }).Control.Visibility = "Hidden"
