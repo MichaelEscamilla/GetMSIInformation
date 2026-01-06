cb9d8f20cf107222d4c4a0463a1e0f29526cd90a
Michael Escamilla <MichaelEscamilla@protonmail.com>
2026-01-05 22:43:50 -0800

    Running from Internet Fix

--- GetMSIInformation.ps1
+++ GetMSIInformation.ps1
@@ -399,7 +399,8 @@ function Build-IconImageControls {
         $ToolTip.Content = "Click to Export.`nUse PowerShell 7.4 or higher for better quality."
         $ToolTip.Background = [System.Windows.Media.SolidColorBrush]::new([System.Windows.Media.Colors]::LightGoldenrodYellow)
         $ToolTip.Foreground = [System.Windows.Media.SolidColorBrush]::new([System.Windows.Media.Colors]::Red)
-      } else { $ToolTip.Content = "Click to Export"}
+      }
+      else { $ToolTip.Content = "Click to Export" }
       $ImageControlIcon.ToolTip = $ToolTip
       $ImageControlIcon.SetValue([System.Windows.Controls.ToolTipService]::InitialShowDelayProperty, 100)
       $ImageControlIcon.HorizontalAlignment = "Center"
@@ -595,7 +596,12 @@ function Invoke-LaunchAsPwsh {
       else {
         # Start PowerShell and run the script
         Write-Host "Relaunching script in PowerShell (pwsh.exe)..."
-        Start-Process -FilePath $Script:PowerShellPath -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Minimized -File `"$PSCommandPath`" -FilePath `"$FilePath`""
+        if ($PSCommandPath -ne "") {
+          Start-Process -FilePath $Script:PowerShellPath -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Minimized -File `"$PSCommandPath`" -FilePath `"$FilePath`""
+        }
+        else {
+          Start-Process -FilePath $Script:PowerShellPath -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Minimized -Command `"Invoke-Expression (Invoke-RestMethod 'https://raw.githubusercontent.com/MichaelEscamilla/GetMSIInformation/main/GetMSIInformation.ps1')`""
+        }
         Exit
       }
     }
